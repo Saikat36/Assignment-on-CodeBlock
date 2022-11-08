9f35d5df75bb427fa3df07f836a83a000a62524b
Saikat36 <saikatp360@gmail.com>
2022-11-08 11:18:01 +0530

    added

--- Assignment -3/7.c
+++ Assignment -3/7.c
@@ -2,3 +2,33 @@
 // 7. Write a program to check whether roots of a given quadratic equation
 //    are real & distinct, real & equal or imaginary roots
 
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    // quadratic equation -  ax^2 + bx + c = 0
+
+    double a,b,c,discriminant;
+    printf("Enter 3 number: ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    // In the case of a quadratic equation ax^2 + bx + c = 0,
+    // the discriminant is = b^2 − 4*a*c
+
+    discriminant = (b*b) - (4*a*c);
+
+    if( discriminant > 0 )
+    {
+        printf("Real and discriminant");
+    }
+    else if( discriminant == 0 )
+    {
+        printf("Real and equal");
+    }
+    else
+    {
+        printf("imaginary roots");
+    }
+    return 0;
+}
--- Assignment -3/9.c
+++ Assignment -3/9.c
@@ -1,3 +1,31 @@
 
 // 9. Write a program to find the greatest among three given numbers.
 //    Print number once if the greatest number appears two or three times.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter 3 Numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if((a > b) && (a > c))
+    {
+        printf("%d, is greatest",a);
+    }
+    else if((b > a) && (b > c))
+    {
+        printf("%d, is greatest",b);
+    }
+    else if((c > a) && (c > b))
+    {
+        printf("%d, is greatest",c);
+    }
+    else if((a==b) || (b==c) || (c==a))
+    {
+        printf("Greatest Number is Repeated");
+    }
+    return 0;
+}
