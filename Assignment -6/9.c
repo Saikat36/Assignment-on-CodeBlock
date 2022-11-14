b2915d3a970f841040a9985c0a4a2dd3177b117d
Saikat36 <saikatp360@gmail.com>
2022-11-14 16:47:00 +0530

    added

--- Assignment -6/9.c
+++ Assignment -6/9.c
@@ -1,3 +1,5 @@
+
+
 /* Write  programe to calculate LCM of two numbers */
 
 #include <stdio.h>
@@ -5,18 +7,40 @@
 
 int main()
 {
-    int x, y, store, lcm;
+    int x, y;
     printf("Enter two number: ");
     scanf("%d %d", &x, &y);
-    for (int i = 1; i <= x && i <= y; i++)
+    for (int i = 1; i <= x * y; i++)
     {
-        if (x % i == 0 && y % i == 0)
+        if (i % x == 0 && i % y == 0)
         {
-            store = i;
+            printf("The LCM of two numbers %d and %d is %d.", x, y, i);
+            break;
         }
     }
-    lcm = (x * y) / store;
-    printf("The LCM of two numbers %d and %d is %d.", x, y, lcm);
     printf("\n");
     return 0;
 }
+
+
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main()
+// {
+//     int x, y, store, lcm;
+//     printf("Enter two number: ");
+//     scanf("%d %d", &x, &y);
+//     for (int i = 1; i <= x && i <= y; i++)
+//     {
+//         if (x % i == 0 && y % i == 0)
+//         {
+//             store = i;
+//         }
+//     }
+//     lcm = (x * y) / store;
+//     printf("The LCM of two numbers %d and %d is %d.", x, y, lcm);
+//     printf("\n");
+//     return 0;
+// }
