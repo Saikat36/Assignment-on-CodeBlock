c89beb89669373e0751aecd8e2d02de407cd343c
Saikat36 <saikatp360@gmail.com>
2022-11-30 21:13:56 +0530

    added

---
+++ Assignment -12/7.c
@@ -0,0 +1,18 @@
+
+
+/* 7. Write a recursive function to print squares of first N natural numbers */
+
+#include <stdio.h>
+
+int main()
+{
+    int N, square = 1;
+    printf("Enter a number: ");
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++)
+    {
+        square = square * (i * i);
+    }
+    printf("squares of first %d natural number is: %d", N,square);
+    return 0;
+}
