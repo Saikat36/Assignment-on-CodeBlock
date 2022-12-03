938113c5057c0762701f3833dffcf75e3aa1ad85
Saikat36 <saikatp360@gmail.com>
2022-12-03 17:46:33 +0530

    added

--- Assignment -14/p-1.c
+++ Assignment -14/p-1.c
@@ -1,22 +1,37 @@
 
+/* Write a function to add 2 array  */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int a[3][3] = {1,2,3,4,5,3,4,2,6};
-    int b[3][3] = {3,4,5,3,4,2,6,0,5};
-    int c[3][3];
-    int i,j;
+    int a[3][3] = {1, 2, 3,
+                   4, 5, 3,
+                   4, 2, 6};
 
-    for (i = 0; i <3; i++)
+    int b[3][3] = {3, 4, 5,
+                   3, 4, 2, 
+                   6, 0, 5};
+
+    int c[3][3];
+    int i, j;
+
+    for (i = 0; i < 3; i++)
     {
-        for (j = 0; j <3; j++)
+        for (j = 0; j < 3; j++)
         {
-            c[i][j] = a[i][j]+b[i][j];
+            c[i][j] = a[i][j] + b[i][j];
         }
-        printf("%d\n",c[i][j]);
     }
-    
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
