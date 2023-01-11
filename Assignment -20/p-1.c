5b7ef92b73405832e3980391d0df0b1335a8fd47
Saikat36 <saikatp360@gmail.com>
2023-01-11 13:37:00 +0530

    added

--- Assignment -20/p-1.c
+++ Assignment -20/p-1.c
@@ -4,20 +4,22 @@
 
 int main()
 {
-    int x = 10, y = 5;
-    int *r = &x , *s = &y;
+    int x = 10;
+    int *r = &x ;
 
     printf("\n\n");
-    printf("*r = %d \n",*r);
-    printf("++(*r) = %d \n",++(*r));
-    printf("++*r = %d \n",++*r);
-    printf("*++r = %d \n",*++r);
-    printf("*r++ = %d \n",*r++);
 
-   /*    *r = 10 chilo but ato bar r ar address a store kora value ta agea daya hoa6a akon " &x " orthat *p ar jaigai null value = 0 asa ga6a tai jokon *r value print kora hobe tokon *r =0 hoba onno value add hobar por*r = 34;  */
+    printf("*r = %d \n",*r);           //   *r = 10
+    printf("r = %u \n",r);             //   r = 6422028
+    printf("++(*r) = %d \n",++(*r));   //   ++(*r) = 11
+    printf("r = %d \n",r);             //   r = 6422028
+    printf("++*r = %d \n",++*r);       //   ++*r = 12
+    printf("*++r = %d \n",*++r);       //   *++r = 6422024
+    printf("*r++ = %d \n",*r++);       //   *r++ = 6422024
 
-    printf("*r = %d \n\n",*r);  //  *r = 0 karon ta ai uporer rules tar jonno
-    printf("y = %d \n",y);
+   /*    *r = 10 chilo but ato bar r ar address a store kora value ta agea daya hoa6a akon " &x " orthat *p ar jaigai null value = 0 asa ga6a tai jokon *r value print kora hobe tokon *r =0 hoba onno value add hobar por *r = 34;  */
+
+    printf("*r = %d \n\n",*r);         //   *r = 0 karon ta ai uporer rules tar jonno
 
     return 0;
 }
