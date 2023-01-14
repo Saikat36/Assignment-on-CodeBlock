db6deaae599c0aafcb061ba7e64c1662d4f88fc0
Saikat36 <saikatp360@gmail.com>
2023-01-14 15:26:08 +0530

    added

--- Assignment -18/p-1.c
+++ Assignment -18/p-1.c
@@ -1,2 +1,20 @@
 
 
+#include<stdio.h>
+
+int main()
+{
+    int x = 5;
+    char a = 'b';
+
+    int *p = &x;
+    char *q = &a;
+
+    printf("\nValue at x = %d , Address of x = %p\n",*&x,&x);
+    printf("\nValue at x == *p = %d , Address of x == p = %p\n",*p,p);
+
+    printf("\nValue at a = %c , Address of a = %p\n",a,&a);
+    printf("\nValue at a == *q = %c , Address of a == q = %p\n\n",*q,q);
+
+    return 0;
+}
