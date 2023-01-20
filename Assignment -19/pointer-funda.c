12a5cfe5e25012770f0fe28d20f7b5ddfce6a166
Saikat36 <saikatp360@gmail.com>
2023-01-20 11:14:55 +0530

    added

--- Assignment -19/pointer-funda.c
+++ Assignment -19/pointer-funda.c
@@ -7,6 +7,11 @@ void input(int *p, int size);
 int main()
 {
     int a[10];
+
+    printf("\na[0] = %d\n",a[0]);
+    printf("&a[0] = %d\n",&a[0]);
+
+
     input(a,10);
     printf("\n\n");
     display(a,10);
@@ -25,7 +30,16 @@ void display(int *p, int size)
 
 void input(int *p, int size)
 {
-    printf("Enter %d numbers\n", size);
+    printf("*p = %d\n",*p);
+    printf("*p+1 = %d\n",*p+1);
+    printf("p = %d = &a[0] = Address of a[0]\n",p);
+    printf("p[0] = %d = a[0] = value at a[0]\n",p[0]);
+    printf("p[i] = value at a[i]\n");
+    printf("p+1 = %d\n",p+1);
+    printf("&p[0] = %d\n",&p[0]);
+    printf("&p[1] = %d\n",&p[1]);
+
+    printf("\nEnter %d numbers\n", size);
 
     for (int i = 0; i < size; i++)
     {
